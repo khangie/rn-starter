cb02c3dc231e7c524d3bb9a0716e3b59acc393ed
Khang Nguyen <khangie@hotmail.com>
2021-10-30 14:04:00 -0700

    added age

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -5,15 +5,13 @@ import { FlatList } from 'react-native-gesture-handler';
 const ListScreen = () => {
 
     const friends = [
-        { name : 'Friend #1' },
-        { name : 'Friend #2' },
-        { name : 'Friend #3' },
-        { name : 'Friend #4' },
-        { name : 'Friend #5' },
-        { name : 'Friend #6' },
-        { name : 'Friend #7' },
-        { name : 'Friend #8' },
-        { name : 'Friend #9' }
+        { name : 'Friend #1', age: '20' },
+        { name : 'Friend #2', age: '45'  },
+        { name : 'Friend #3', age: '32'  },
+        { name : 'Friend #4', age: '27'  },
+        { name : 'Friend #5', age: '53'  },
+        { name : 'Friend #6', age: '30'  },
+        { name : 'Friend #7', age: '20'  }
     ];
 
     return (
@@ -21,7 +19,7 @@ const ListScreen = () => {
             keyExtractor={(friend) => friend.name}
             data={friends} 
             renderItem={({ item }) => {
-                return <Text>{item.name}</Text>;
+                return <Text style={styles.textStyle}>{item.name} - {item.age}</Text>;
             }}
         
         />
@@ -29,6 +27,10 @@ const ListScreen = () => {
 
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    textStyle: {
+        marginVertical: 50
+    }
+});
 
 export default ListScreen;
