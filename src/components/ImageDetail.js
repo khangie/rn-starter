c663705b7f91776f649d9c7bf90742408533908f
Khang Nguyen <knguyen@Khangs-MBP.lan>
2021-11-06 11:59:18 -0700

    image score

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
 const ImageDetail = (props) => {
+    console.log(props);
     return ( 
         <View>
-            <Image source={require('../../assets/beach.jpg')}/>
+            <Image source={props.imageSource}/>
             <Text>{props.title}</Text>
+            <Text>Image Score - {props.imageScore}</Text>
         </View>
     );
 };
