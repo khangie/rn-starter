e56199156ce1ecc96e39b71ecbb1800b8c254936
Khang Nguyen <knguyen@Khangs-MacBook-Pro.local>
2021-11-20 13:54:01 -0800

    Added state to TextScreen

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useState } from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 const TextScreen = () => {
+
+    const [name, setName] = useState('');
+
     return (
         <View>
-            <Text>Text Screen</Text>
+            <Text>Enter Name</Text>
+            <TextInput 
+                style={styles.Input}
+                autoCapitalize="none"
+                autoCorrect={false}
+                value={name}
+                onChangeText={(newValue) => setName(newValue)}
+        />
+        <Text>My name is {name}</Text>
         </View>
     );
 
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    Input: {
+        margin: 15,
+        borderColor: 'black',
+        borderWidth: 1,
+        height: 40
+    }
+});
 
 export default TextScreen;
